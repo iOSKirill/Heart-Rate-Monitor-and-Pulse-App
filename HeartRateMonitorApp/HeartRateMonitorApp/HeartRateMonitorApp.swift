6b685f895a70aef5f03dba7cd57deca171b9598c
Kirill Manuilenko <m.kirill4163@gmail.com>
2023-11-28 23:40:04 +0300

    Initial commit

---
+++ HeartRateMonitorApp/HeartRateMonitorApp/HeartRateMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateMonitorAppApp.swift
+//  HeartRateMonitorApp
+//
+//  Created by Kirill Manuilenko on 28.11.23.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
