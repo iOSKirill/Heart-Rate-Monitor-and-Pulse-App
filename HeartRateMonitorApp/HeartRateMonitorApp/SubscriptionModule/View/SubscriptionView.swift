9e40264b9340b29cafbb342191b28eeae68e406b
Kirill Manuilenko <m.kirill4163@gmail.com>
2024-03-05 20:24:48 +0300

    ADDED localizable subscription screen

---
+++ HeartRateMonitorApp/HeartRateMonitorApp/SubscriptionModule/View/SubscriptionView.swift
@@ -0,0 +1,19 @@
+//
+//  SubscriptionView.swift
+//  HeartRateMonitorApp
+//
+//  Created by Kirill Manuilenko on 5.03.24.
+//
+
+import SwiftUI
+
+struct SubscriptionView: View {
+    var body: some View {
+        ZStack {
+        }
+    }
+}
+
+#Preview {
+    SubscriptionView()
+}
---
+++ HeartRateMonitorApp/HeartRateMonitorApp/SubscriptionModule/ViewModel/SubscriptionViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SubscriptionViewModel.swift
+//  HeartRateMonitorApp
+//
+//  Created by Kirill Manuilenko on 5.03.24.
+//
+
+import Foundation
+
+final class SubscriptionViewModel: ObservableObject {
+    // MARK: - Property -
+}
